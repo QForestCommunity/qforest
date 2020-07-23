0fbf74c8ed77da28043c0bd0980ea243857dc0b2
unknown <sashwat.anagolum@gmail.com>
2020-07-23 12:04:54 +0530

    Updates

---
+++ precache-manifest.249660dd6b0fc1eba89f595c859ddf8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ef3a6fc51eab14a33c5eacf193e4dd",
+    "url": "/qforest/index.html"
+  },
+  {
+    "revision": "8fcf88cc9e7c1b16cafc",
+    "url": "/qforest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4702a049191cd279d13a",
+    "url": "/qforest/static/js/2.d89cba6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qforest/static/js/2.d89cba6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fcf88cc9e7c1b16cafc",
+    "url": "/qforest/static/js/main.3b463536.chunk.js"
+  },
+  {
+    "revision": "c2d5970ca85244d40bd1",
+    "url": "/qforest/static/js/runtime-main.5014f9be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qforest/static/media/logo.5d5d9eef.svg"
+  }
+]);
